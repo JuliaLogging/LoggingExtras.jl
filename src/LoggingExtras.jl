b6142532fbd8afe82a9168d96ed5d667695f2178
Lyndon White <oxinabox@ucc.asn.au>
2018-09-19 13:20:03 +0800

    =link it together

--- src/LoggingExtras.jl
+++ src/LoggingExtras.jl
@@ -1,5 +1,15 @@
 module LoggingExtras
 
-greet() = print("Hello World!")
+using Base.CoreLogging:
+    LogLevel, BelowMinLevel, Debug, Info, Warn, Error, AboveMaxLevel,
+
+import Base.CoreLogging:
+    AbstractLogger, SimpleLogger,
+	handle_message, shouldlog, min_enabled_level, catch_exceptions,
+
+
+include("demuxlogger.jl")
+include("filteredlogger.jl")
+include("filelogger.jl")
 
 end # module
