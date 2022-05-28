6b79cacb121bb09051914b3a3cea7fe505f9cda2
Lyndon White <lyndon.white@invenialabs.co.uk>
2022-05-28 16:18:47 +0100

    reorganize all the files
    include common.jl file
    
    include new file locations

--- src/LoggingExtras.jl
+++ src/LoggingExtras.jl
@@ -24,34 +24,20 @@ export Logging, AbstractLogger, LogLevel, NullLogger,
     with_logger, current_logger, global_logger, disable_logging,
     SimpleLogger, ConsoleLogger
 
-######
-# Utilities for dealing with compositional loggers.
-# Since the logging system itself will not engage its checks
-# Once the first logger has started, any compositional logger needs to check
-# before passing anything on.
 
-# For checking child logger, need to check both `min_enabled_level` and `shouldlog`
-function comp_shouldlog(logger, args...)
-    level = first(args)
-    min_enabled_level(logger) <= level && shouldlog(logger, args...)
-end
+include("CompositionalLoggers/common.jl")
+include("CompositionalLoggers/activefiltered.jl")
+include("CompositionalLoggers/earlyfiltered.jl")
+include("CompositionalLoggers/minlevelfiltered.jl")
+include("CompositionalLoggers/overridelogger.jl")
+include("CompositionalLoggers/tee.jl")
+include("CompositionalLoggers/transformer.jl")
 
-# For checking if child logger will take the message you are sending
-function comp_handle_message_check(logger, args...; kwargs...)
-    level, message, _module, group, id, file, line = args
-    return comp_shouldlog(logger, level, _module, group, id)
-end
-###############################
+include("Sinks/formatlogger.jl")
+include("Sinks/filelogger.jl")
+include("Sinks/datetime_rotation.jl")
 
-include("tee.jl")
-include("transformer.jl")
-include("activefiltered.jl")
-include("earlyfiltered.jl")
-include("minlevelfiltered.jl")
-include("filelogger.jl")
-include("formatlogger.jl")
-include("datetime_rotation.jl")
-include("overridelogger.jl")
+
 include("verbosity.jl")
 include("deprecated.jl")
 
